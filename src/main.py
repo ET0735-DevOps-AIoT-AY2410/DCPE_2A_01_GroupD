03a28ef94858c4a67e88837aeceacc25057b4b1a
Jiehao <jiehao.23@ichat.sp.edu.sg>
2024-07-15 15:28:53 +0800

    dispensing book function with lcd, led and motor. not tested yet

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from mongoApi import MongoDB
 from datetime import date, datetime, timedelta
+from time import sleep
+from hal import hal_dc_motor as init_motor, set_motor_speed
+from hal import hal_lcd as init_led, set_led_output
+from hal import hal_lcd as lcd 
+import RPi.GPIO as GPIO
 
 booksDB = MongoDB('books')
 usersDB = MongoDB('users')
@@ -22,17 +27,31 @@ def handlePayment(userId):
 
 
 def borrow_book_from_db(userId):
-    # Find user by id
-    pass
-
-        # Found book reserved
-
-        # DISPENSE THREAD HERE
-
-
+    init_motor()
+    init_led()
+    my_lcd = lcd()
+    my_lcd.lcd_clear()
+    user = usersDB.getItems(filter={"userId": userId})
+    user = user[0]
+    if "reservedBooks" in user and user["reservedBooks"]:
+        reserved_book_id = user["reservedBooks"][0]
+        print(f"Dispensing book with ID: {reserved_book_id}")
+        my_lcd.lcd_display_string("Dispensing Book with ID: {reserved_book_id}", 1) #display on lcd
+        set_motor_speed(100)  # set motor to dispense 
+        set_led_output(GPIO.HIGH) #turn led on
+        sleep(5)
+        set_motor_speed(0)  # to stop the dispensing motor
+        set_led_output(GPIO.LOW)  #turn led off
+        my_lcd.lcd_display_string("Dispensing complete", 1) #display on lcd
+        
         # Update book status
+        booksDB.updateItem({"_id": reserved_book_id}, {"status": "borrowed"}) 
+        usersDB.updateItem({"_id": userId}, {"reservedBooks": user['reservedBooks']})
        
         # Add book to user borrowedBooks
+        usersDB.appendItem({"_id": userId}, {"borrowedBooks": reserved_book_id})
+    else:
+        my_lcd.lcd_display_string("No reservations", 1) #display on lcd
        
 
 def process_bar_code(image):
